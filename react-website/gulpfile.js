594c0d192307a8d12e901573c92a2aac415b3dd7
jade <jadealvares@hotmail.co.uk>
2017-07-30 17:38:23 +0100

    installed dependencies for gulp

--- react-website/gulpfile.js
+++ react-website/gulpfile.js
@@ -1,5 +1,31 @@
-var gulp = require('gulp');
+'use strict';
 
-gulp.task('default', function() {
-  // place code for your default task here
+//dependencies
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var minifyCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var changed = require('gulp-changed');
+
+var SCSS_SRC = './src/Assets/scss/**/*.scss';
+var SCSS_DEST = './src/Assets/css';
+
+//compile SCSS
+gulp.task('compile_scss', function(){
+  gulp.src(SCSS_SRC)
+  .pipe(sass().on('error', sass.logError))
+  .pipe(minifyCSS())
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(changed(SCSS_DEST))
+  .pipe(gulp.dest(SCSS_DEST));
+
 });
+
+//detect changes in SCSS_DEST
+gulp.task('watch_scss', function(){
+  gulp.watch(SCSS_SRC, ['compile_scss']);
+});
+
+//run tasks
+gulp.task('default', ['watch_scss']);
