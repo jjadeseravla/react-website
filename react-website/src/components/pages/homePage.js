98b5e3c667fc695b506b17e2f16b6b15f9bb106a
jade <jadealvares@hotmail.co.uk>
2017-07-31 21:56:01 +0100

    add a footer and homepage

---
+++ react-website/src/components/pages/homePage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Homepage extends Component {
+  render() {
+    return (
+      <div className="wrap">
+
+        Homepage content here
+      </div>
+    );
+  }
+}
+
+export default Homepage;
