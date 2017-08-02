59344ace89e55cf3ffea1462cca2bdd251bc927e
jade <jadealvares@hotmail.co.uk>
2017-08-02 11:00:35 +0100

    implement text on page and hovering over links on menu

--- react-website/src/components/pages/homePage.js
+++ react-website/src/components/pages/homePage.js
@@ -4,8 +4,13 @@ class Homepage extends Component {
   render() {
     return (
       <div className="container-fluid">
+        <h1>
+          Homepage content
+        </h1>
 
-        Homepage content here
+        <p>
+          To understand this question, we need to look at the state of play of AI. Currently, not only can AI tirelessly generate content by monitoring how it is consumed, by whom, where, and the reactions and behavior of the readers, it can also learn from it, filter it into the creative process, become stronger, and more focused. Add into this equation the fact that it never asks for a raise, gets drunk at the Christmas party, expects holidays, or needs to visit the bathroom, and you can see why people are finding it so attractive.
+        </p>
       </div>
     );
   }
